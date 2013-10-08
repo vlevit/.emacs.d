6362b972ab0f394821d8bd87de87040574232e4c
Vyacheslav Levit <levit.slava@gmail.com>
2013-10-08 22:08:24 +0300

    Truncate lines in html by default

--- my-html.el
+++ my-html.el
@@ -1,6 +1,7 @@
 (defun my-html-mode-hook ()
   (auto-fill-mode -1)
-  (setq sgml-basic-offset 4))
+  (setq sgml-basic-offset 4)
+  (setq truncate-lines t))
 
 (add-hook 'html-mode-hook 'my-html-mode-hook)
 
