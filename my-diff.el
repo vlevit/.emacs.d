360a312ff52d202abdebe7db5501f98cdb80c73a
Vyacheslav Levit <levit.slava@gmail.com>
2013-11-25 16:17:06 +0200

    Refine diff hunks by default

--- my-diff.el
+++ my-diff.el
@@ -1,7 +1,9 @@
 (custom-set-faces
  '(diff-added ((t (:background "#DDFFDD"))) 'now)
  '(diff-removed ((t (:background "#FFDDDD"))) 'now)
- '(diff-header ((t (:background "#DDDDFF"))) 'now)
+ '(diff-header ((t (:background "#F8E9AE"))) 'now)
  '(diff-file-header ((t (:weight bold :background "#DDDDFF"))) 'now))
+
+(add-hook 'diff-mode-hook 'diff-auto-refine-mode)
 
 (provide 'my-diff)
--- my-git.el
+++ my-git.el
@@ -1,6 +1,9 @@
-(when (require 'magit nil 'noerror)
-  (global-set-key (kbd "C-x g") 'magit-status)
-  (global-set-key (kbd "C-x C-g") 'magit-status))
+(require 'magit)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x C-g") 'magit-status)
+
+(setq magit-diff-refine-hunk t)
 
 (require 'git-gutter-fringe)
 (global-git-gutter-mode t)
