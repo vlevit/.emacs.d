fb07e36cb375a89a6b77471566f17a584dcaad49
Vyacheslav Levit <levit.slava@gmail.com>
2013-11-25 14:08:21 +0200

    Setup git-gutter-fringe

--- my-git.el
+++ my-git.el
@@ -2,4 +2,11 @@
   (global-set-key (kbd "C-x g") 'magit-status)
   (global-set-key (kbd "C-x C-g") 'magit-status))
 
+(require 'git-gutter-fringe)
+(global-git-gutter-mode t)
+
+;; Jump to next/previous hunk
+(global-set-key (kbd "C-c p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-c n") 'git-gutter:next-hunk)
+
 (provide 'my-git)
