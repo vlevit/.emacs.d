b95e37d8cf524eabc1d9ab5cb41bc0fbbf0d37eb
Vyacheslav Levit <levit.slava@gmail.com>
2013-11-27 13:18:59 +0200

    Configure smartparens: disable quoting and highlighting

--- my-smartparens.el
+++ my-smartparens.el
@@ -3,6 +3,8 @@
 (require 'smartparens-config)
 (smartparens-global-mode t)
 
+(setq sp-autoescape-string-quote nil)
+(setq sp-highlight-pair-overlay nil)
 ;;; re-wrap region with any pair
 (setq sp-wrap-repeat-last 2)
 
