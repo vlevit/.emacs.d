5cdddbd47c4a3b8184d330ab8d5e149008573a29
Vyacheslav Levit <levit.slava@gmail.com>
2013-12-13 12:11:36 +0200

    Ido: go to home and root just by typing ~ and /

--- my-ido.el
+++ my-ido.el
@@ -105,9 +105,29 @@
 (setq ido-decorations (quote (" " "" "\n              " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
 (defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
 (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+
+(defun ido-go-home ()
+  (interactive)
+  (ido-set-current-home)
+  (setq ido-exit 'refresh
+        ido-text-init ido-text
+        ido-rotate-temp t)
+  (exit-minibuffer))
+
+(defun ido-go-root ()
+  (interactive)
+  (ido-set-current-directory "/")
+  (setq ido-exit 'refresh
+        ido-text-init ido-text
+        ido-rotate-temp t)
+  (exit-minibuffer))
+
 (defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
   (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
-  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
+  (define-key ido-file-completion-map (kbd "~") 'ido-go-home)
+  (define-key ido-file-completion-map (kbd "/") 'ido-go-root))
+
 (add-hook 'ido-setup-hook 'ido-define-keys)
 
 (global-set-key (kbd "M-`") 'ido-switch-buffer)
