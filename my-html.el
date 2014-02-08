e9dd65294db5183e460e90af76b04f6f59e32c8d
Vyacheslav Levit <levit.slava@gmail.com>
2014-02-08 17:20:53 +0200

    Set 2 spaces for html tab width

--- my-html.el
+++ my-html.el
@@ -1,6 +1,6 @@
 (defun my-sgml-mode-hook ()
   (auto-fill-mode -1)
-  (setq sgml-basic-offset 4)
+  (setq sgml-basic-offset 2)
   (setq truncate-lines t))
 
 (add-hook 'sgml-mode-hook 'my-sgml-mode-hook)
