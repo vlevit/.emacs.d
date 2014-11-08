4b2c4d85b2f72c06579ac82d65e6a2e537cc7228
Vyacheslav Levit <levit.slava@gmail.com>
2014-11-09 01:46:45 +0200

    Split vlblog into site-lisp and setup-lisp

--- setup-lisp/setup-vlblog.el
+++ setup-lisp/setup-vlblog.el
@@ -1,106 +1,20 @@
-(require 's)
+(require 'vlblog)
 
-(defvar blog-file-p nil)
+(setq vlblog-content-dir "~/projects/vlevit.org/content/")
 
-(defun vlblog-parse-help-string (str)
-  "Parse a single string STR from `vlblog help' output.
-Return nil if STR is not a command.
-Return a list of commands and options otherwise."
-  (let ((matches (s-match "^vlblog \\(\\S +\\) ?\\(.+\\)*" str)))
-    (if (<= (length matches) 2)
-        (cdr matches)
-      (cons (nth 1 matches) (s-split "|" (nth 2 matches) t)))))
+(dir-locals-set-class-variables
+ 'my-blog
+ '((markdown-mode . ((vlblog-file-p . t)))))
 
-(defun vlblog-commands ()
-  "Run `vlblog help' an return a list of lists whose car is a
-command and whose cdr is options."
-  (delq nil (mapcar 'vlblog-parse-help-string
-                    (mapcar 's-trim-left
-                            (s-lines (shell-command-to-string "vlblog help"))))))
-
-(defun vlblog ()
-  "Run vlblog commands like in shell but with ido autocompletions."
-  (interactive)
-  (let* ((commands (vlblog-commands))
-         (commands-flat (mapcar 'car commands))
-         (command (ido-completing-read "vlblog " commands-flat))
-         (options (cdr (assoc command commands)))
-         (option nil))
-    (when options
-      (setq option (ido-completing-read (concat "vlblog " command " ") options)))
-    (async-shell-command (s-join " " (list "vlblog" command option)))))
-
-(defun blog-file-p ()
-  (string-prefix-p
-   (expand-file-name "~/projects/vlevit.org/content/") (buffer-file-name)))
-
-(defun vlblog-update ()
-  "Run a local server if it is not running yet and update the blog"
-  (interactive)
-  (if (/= (call-process "vlblog" nil nil nil "active") 0)
-      (progn (async-shell-command "vlblog run" "*vlblog*")
-             (with-current-buffer "*vlblog*" (setq truncate-lines t))
-             (run-at-time "4 sec" nil '(lambda () (shell-command "vlblog update local"))))
-    (shell-command "vlblog update local")))
-
-(defun vlblog-view ()
-  "Run local server if it is not running and update the blog"
-  (interactive)
-  (call-process "vlblog" nil nil nil
-                "view" (buffer-file-name) (number-to-string (line-number-at-pos))))
-
-(require 'magit)
-
-(defun vlblog-staged-files ()
-  (let ((diff-output (magit-git-string "diff" "--name-only" "--cached")))
-    (when diff-output
-      (split-string diff-output "\n"))))
-
-(defun vlblog-commit-file ()
-  (interactive)
-  (let ((top-dir (magit-get-top-dir)))
-        (if (not top-dir)
-            (message "File is not in a git repo")
-          (let ((staged-files (vlblog-staged-files)))
-            (if (or (not staged-files)
-                    (equal
-                     (list (substring (buffer-file-name) (length top-dir)))
-                     staged-files))
-                (progn
-                  (magit-run-git "add" (buffer-file-name))
-                  (magit-log-edit-append
-                   (format "Content: %s blog" (vblog-from-filename (buffer-file-name))))
-                  (magit-log-edit))
-              (message "There are some unstaged files"))))))
-
-(defun vlblog-publish ()
-  (interactive)
-  (async-shell-command "vlblog publish" "*vlblog-publish*")
-  (with-current-buffer "*vlblog-publish*" (setq truncate-lines t)))
-
-(defun vlblog-update-on-save ()
-  (when blog-file-p (vlblog-update)))
-
-(add-hook 'after-save-hook 'vlblog-update-on-save)
-
-(defun vblog-from-filename (filename)
-  (string-match "/blog/.\\{2\\}/\\([0-9a-zA-Z-]+\\)/" filename)
-  (match-string 1 filename))
+(dir-locals-set-directory-class vlblog-content-dir 'my-blog)
 
 (defun vlblog-hook ()
-  (when (blog-file-p)
+  (when (vlblog-file-p)
     (local-set-key (kbd "C-c C-u") 'vlblog-update)
     (local-set-key (kbd "C-c C-v") 'vlblog-view)
     (local-set-key (kbd "C-c C-m") 'vlblog-commit-file)
     (local-set-key (kbd "C-c C-l") 'vlblog-publish)))
 
 (add-hook 'markdown-mode-hook 'vlblog-hook)
-
-(dir-locals-set-class-variables
- 'my-blog
- '((markdown-mode . ((blog-file-p . t)))))
-
-(dir-locals-set-directory-class
- "~/projects/vlevit.org/content/" 'my-blog)
 
 (provide 'setup-vlblog)
---
+++ site-lisp/vlblog.el
@@ -0,0 +1,100 @@
+(require 's)
+(require 'magit)
+
+(defvar vlblog-file-p nil)
+(defvar vlblog-content-dir nil)
+(defvar vlblog-log-message nil)
+
+(defun vlblog-parse-help-string (str)
+  "Parse a single string STR from `vlblog help' output.
+Return nil if STR is not a command.
+Return a list of commands and options otherwise."
+  (let ((matches (s-match "^vlblog \\(\\S +\\) ?\\(.+\\)*" str)))
+    (if (<= (length matches) 2)
+        (cdr matches)
+      (cons (nth 1 matches) (s-split "|" (nth 2 matches) t)))))
+
+(defun vlblog-commands ()
+  "Run `vlblog help' an return a list of lists whose car is a
+command and whose cdr is options."
+  (delq nil (mapcar 'vlblog-parse-help-string
+                    (mapcar 's-trim-left
+                            (s-lines (shell-command-to-string "vlblog help"))))))
+
+(defun vlblog ()
+  "Run vlblog commands like in shell but with ido autocompletions."
+  (interactive)
+  (let* ((commands (vlblog-commands))
+         (commands-flat (mapcar 'car commands))
+         (command (ido-completing-read "vlblog " commands-flat))
+         (options (cdr (assoc command commands)))
+         (option nil)
+         (buffer (if (equal command "start") "*vlblog-server*" "*vlblog*")))
+    (when options
+      (setq option (ido-completing-read (concat "vlblog " command " ") options)))
+    (async-shell-command (s-join " " (list "vlblog" command option)) (get-buffer-create buffer))))
+
+(defun vlblog-file-p ()
+  (and vlblog-content-dir
+       (string-prefix-p (expand-file-name vlblog-content-dir) (buffer-file-name))))
+
+(defun vlblog-update ()
+  "Run a local server if it is not running yet and update the blog"
+  (interactive)
+  (if (/= (call-process "vlblog" nil nil nil "active") 0)
+      (progn (async-shell-command "vlblog start" "*vlblog-server*")
+             (with-current-buffer "*vlblog-server*" (setq truncate-lines t))
+             (run-at-time "4 sec" nil '(lambda () (shell-command "vlblog update local"))))
+    (shell-command "vlblog update local")))
+
+(defun vlblog-view ()
+  "Run local server if it is not running and update the blog"
+  (interactive)
+  (call-process "vlblog" nil nil nil
+                "view" (buffer-file-name) (number-to-string (line-number-at-pos))))
+
+(defun vlblog-staged-files ()
+  (let ((diff-output (magit-git-string "diff" "--name-only" "--cached")))
+    (when diff-output
+      (split-string diff-output "\n"))))
+
+(defun vlblog-commit-file ()
+  (interactive)
+  (let ((top-dir (magit-get-top-dir)))
+    (if (not top-dir)
+        (message "File is not in a git repo")
+      (let ((staged-files (vlblog-staged-files))
+            (log-message (format "Content: %s blog" (vblog-from-filename (buffer-file-name)))))
+        (if (and staged-files
+                 (not (equal (list (substring (buffer-file-name) (length top-dir))) staged-files)))
+            (message "There are some uncommited files")
+          (magit-run-git "add" (buffer-file-name))
+          (kill-new log-message)
+          (setq vlblog-log-message log-message)
+          (with-temp-buffer
+            (cd top-dir)
+            (magit-commit)))))))
+
+(defun vlblog-log-insert-message ()
+  (message "vlblog-log-insert-message")
+  (message vlblog-log-message)
+  (insert vlblog-log-message)
+  (setq vlblog-log-message nil))
+
+(add-hook 'git-commit-mode-hook 'vlblog-log-insert-message)
+
+(defun vlblog-publish ()
+  (interactive)
+  (async-shell-command "vlblog publish" "*vlblog-publish*")
+  (with-current-buffer "*vlblog-publish*" (setq truncate-lines t)))
+
+(defun vlblog-update-on-save ()
+  (when vlblog-file-p (vlblog-update)))
+
+(add-hook 'after-save-hook 'vlblog-update-on-save)
+
+(defun vblog-from-filename (filename)
+  (string-match "/blog/.\\{2\\}/\\([0-9a-zA-Z-]+\\)/" filename)
+  (match-string 1 filename))
+
+(provide 'vlblog)
