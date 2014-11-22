762cc283c9a38157d4b70f4ea44929589c59b4f0
Vyacheslav Levit <levit.slava@gmail.com>
2014-11-22 23:30:40 +0200

    Enable cua-selection-mode, provide cua-replace-region for smartparens

--- setup-lisp/setup-smartparens.el
+++ setup-lisp/setup-smartparens.el
@@ -8,4 +8,18 @@
 ;;; re-wrap region with any pair
 (setq sp-wrap-repeat-last 2)
 
+;; smartparens depends on cua-replace-region which was removed in Emacs 24.4
+;; https://github.com/Fuco1/smartparens/issues/271
+(unless (fboundp 'cua-replace-region)
+  (defun cua-replace-region ()
+    "Replace the active region with the character you type."
+    (interactive)
+    (let ((not-empty (and cua-delete-selection (cua-delete-region))))
+      (unless (eq this-original-command this-command)
+        (let ((overwrite-mode
+               (and overwrite-mode
+                    not-empty
+                    (not (eq this-original-command 'self-insert-command)))))
+          (cua--fallback))))))
+
 (provide 'setup-smartparens)
