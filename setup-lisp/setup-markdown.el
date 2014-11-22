f4a970a6fce30c37e3d60bc2ad5678b4359c770a
Vyacheslav Levit <levit.slava@gmail.com>
2014-11-22 23:19:45 +0200

    Update monkey-patch for markdown to support vlblog headers

--- setup-lisp/setup-markdown.el
+++ setup-lisp/setup-markdown.el
@@ -2,13 +2,22 @@
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 
+(defconst markdown-regex-multimarkdown-metadata
+  "^/?\\([[:alpha:]][[:alpha:] _-]*?\\):[ \t]*\\(.*\\)$"
+  "Regular expression for matching MultiMarkdown metadata.")
+
 (defconst markdown-regex-header
-  "#+\\|/title: \\|\\S-.*\n\\(?:\\(===+\\)\\|\\(---+\\)\\)$"
+  "^\\(?:\\(/title:\\)\\s-*\\(.*\\)\\|\\(.+\\)\n\\(=+\\)\\|\\(.+\\)\n\\(-+\\)\\|\\(#+\\)\\s-*\\(.*?\\)\\s-*?\\(#*\\)\\)$"
   "Regexp identifying Markdown headers.")
 
-(defconst markdown-regex-header-1-atx
-  "^\\(# \\|/title: \\)\\(.*?\\)\\($\\| #+$\\)"
-  "Regular expression for level 1 atx-style (hash mark) headers.")
+(defconst markdown-regex-title
+  "^\\(/title:\\)[ \t]*\\(.*\\)$"
+  "Regular expression for generic /title:-style headers.")
+
+(add-to-list 'markdown-mode-font-lock-keywords-basic
+             (cons markdown-regex-title '((1 markdown-header-delimiter-face)
+                                          (2 markdown-header-face-1))))
+(markdown-reload-extensions)
 
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
