dfc70d9aaec1e3499ed4f254e41371484e9bf21d
Vyacheslav Levit <levit.slava@gmail.com>
2014-11-22 23:19:05 +0200

    Bind end-line-and-newline-and-indent to M-o

--- setup-lisp/setup-end-line-newline.el
+++ setup-lisp/setup-end-line-newline.el
@@ -3,6 +3,6 @@
   (end-of-line)
   (newline-and-indent))
 
-(global-set-key (kbd "M-B") 'end-line-and-newline-and-indent)
+(global-set-key (kbd "M-o") 'end-line-and-newline-and-indent)
 
 (provide 'setup-end-line-newline)
