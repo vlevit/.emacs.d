4e349cf2c651f1211d6dcee11081c797facbeddf
Vyacheslav Levit <levit.slava@gmail.com>
2014-12-07 01:31:44 +0200

    Rebind elfeed to qk (ql is frequently used because of sql)

--- setup-lisp/setup-chord-keys.el
+++ setup-lisp/setup-chord-keys.el
@@ -32,7 +32,7 @@
 (key-seq-define-global "qg" 'magit-status)
 (key-seq-define-global "qs" 'idomenu)
 (key-seq-define-global "qb" 'ido-switch-buffer)
-(key-seq-define-global "ql" 'elfeed)
+(key-seq-define-global "qk" 'elfeed)
 
 (key-seq-define-global ",s" 'transpose-symbols)
 (key-seq-define-global ",w" 'transpose-words)
