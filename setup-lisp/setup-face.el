bfb2d61d9b4cee943d706ab29312e30733114f1a
Vyacheslav Levit <levit.slava@gmail.com>
2014-12-07 01:29:37 +0200

    Make font smaller

--- setup-lisp/setup-face.el
+++ setup-lisp/setup-face.el
@@ -1,5 +1,5 @@
 (if (>= (x-display-pixel-width) 1920)
-    (set-frame-font "DejaVu Sans Mono 13")
+    (set-frame-font "DejaVu Sans Mono 11")
   (set-frame-font "DejaVu Sans Mono 10")
   (set-face-attribute 'default nil :height 105))
 
