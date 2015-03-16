0d4f1f2fba63434e90c2e0fb8487ae655d13423c
Vyacheslav Levit <dev@vlevit.org>
2015-03-16 11:57:01 +0200

    Read twitter Oauth key from the plain text file.
    Since GPG2 & Gnome-Keyring integration seems to be broken.

--- setup-lisp/setup-twitter.el
+++ setup-lisp/setup-twitter.el
@@ -1,4 +1,13 @@
+(require 'twittering-mode)
+
+(defun twittering-read-from-encrypted-file (file)
+  "Read credentials from the plain text file."
+  (with-temp-buffer
+    (insert-file-contents file)
+    (buffer-string)))
+
 (setq twittering-use-master-password t
+      twittering-private-info-file (expand-file-name "~/.emacs.d/.twittering-mode")
       twittering-icon-mode t
       twittering-status-format "%i %s,  %@: %FACE[font-lock-comment-face]{// from %f%L%r%R}\n\n%FOLD[         ]{%t}\n\n"
       twittering-fill-column 80)
