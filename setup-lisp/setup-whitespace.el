0858f5996aee7b35bfefa79dac339e23646ce024
Vyacheslav Levit <dev@vlevit.org>
2015-10-21 00:12:36 +0300

    Underline tabs in whitespace mode

--- setup-lisp/setup-whitespace.el
+++ setup-lisp/setup-whitespace.el
@@ -11,7 +11,7 @@
 (eval-after-load 'whitespace
   (lambda ()
     (set-face-attribute 'whitespace-newline nil :foreground "#d3d7cf")
-    (set-face-attribute 'whitespace-tab nil :background "#eeeeec")
+    (set-face-attribute 'whitespace-tab nil :background nil :underline "#d3d7cf")
     (set-face-attribute 'whitespace-trailing nil :background nil :underline "#a40000")))
 
 (defun whitespace-post-command-hook() nil) ; workaround for cursor slowdown
