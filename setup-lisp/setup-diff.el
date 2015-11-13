5a8c55eba797b9fa594056f2e00d2bfef024d9df
Vyacheslav Levit <dev@vlevit.org>
2015-11-13 22:24:35 +0200

    Update diff colors

--- setup-lisp/setup-diff.el
+++ setup-lisp/setup-diff.el
@@ -1,8 +1,9 @@
 (custom-set-faces
- '(diff-added ((t (:background "#DDFFDD" :inverse-video nil))) 'now)
- '(diff-removed ((t (:background "#FFDDDD" :inverse-video nil))) 'now)
- '(diff-header ((t (:background "#F8E9AE"))) 'now)
- '(diff-file-header ((t (:weight bold :background "#DDDDFF"))) 'now))
+ '(diff-added ((t (:background "#DDFFDD" :inverse-video nil :foreground "#657b83" :weight normal))))
+ '(diff-file-header ((t (:weight bold :background "#DDDDFF"))))
+ '(diff-header ((t (:background "#F8E9AE"))))
+ '(diff-refine-change ((t (:foreground "#657b83" :weight normal))) t)
+ '(diff-removed ((t (:background "#FFDDDD" :inverse-video nil :foreground "#657b83" :weight normal)))))
 
 (add-hook 'diff-mode-hook 'diff-auto-refine-mode)
 
