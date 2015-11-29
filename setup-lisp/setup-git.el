68962f1905f60ea8710af07577284da9fb4410c9
Vyacheslav Levit <dev@vlevit.org>
2015-11-29 23:43:51 +0200

    Use `1, `2, `3 to show different levels in magit

--- setup-lisp/setup-git.el
+++ setup-lisp/setup-git.el
@@ -12,4 +12,8 @@
 (define-key magit-mode-map (kbd "M-RET")
   '(lambda () (interactive) (magit-visit-item 1)))
 
+(key-seq-define magit-mode-map "`1" 'magit-show-level-1-all)
+(key-seq-define magit-mode-map "`2" 'magit-show-level-2-all)
+(key-seq-define magit-mode-map "`3" 'magit-show-level-3-all)
+
 (provide 'setup-git)
