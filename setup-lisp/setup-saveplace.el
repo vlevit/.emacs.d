3b161091be1022106969ef342decac88f11997b6
Vyacheslav Levit <dev@vlevit.org>
2016-10-04 18:11:15 +0300

    Update saveplace usage for Emacs 25 behavior

--- setup-lisp/setup-saveplace.el
+++ setup-lisp/setup-saveplace.el
@@ -1,5 +1,8 @@
+(require 'saveplace)                          ;; get the package
+
 (setq save-place-file "~/.emacs.d/saveplace") ;; keep my ~/ clean
 (setq-default save-place t)                   ;; activate it for all buffers
-(require 'saveplace)                          ;; get the package
+
+(save-place-mode 1)
 
 (provide 'setup-saveplace)
