ed2f5648012f24f1ebb34f8916a7e3c03a1c1ecb
Vyacheslav Levit <dev@vlevit.org>
2018-07-27 12:26:12 +0300

    Configuration for find-file-in-repository and find-project

---
+++ setup-lisp/setup-find-file-in-repository.el
@@ -0,0 +1,4 @@
+(eval-after-load 'find-file-in-repository
+  (lambda () (setq ffir-completion 'ivy)))
+
+(provide 'setup-find-file-in-repository)
---
+++ setup-lisp/setup-find-project.el
@@ -0,0 +1,19 @@
+(require 'find-project)
+
+(setq find-project-patterns
+      '((:pattern "~/.config" :action find-file-in-repository)
+        "~/.emacs.d"
+        "~/.oh-my-zsh"
+        "~/.emacs.d/site-lisp/*"
+        "~/projects/*"
+        ;; (:function (find-project-traverse "~/projects" 1 ".git") :action magit-status)
+        (:pattern "~/work/*/*")))
+
+(setq find-project-completing-read-projects 'ivy-completing-read)
+(setq find-project-completing-read-actions 'ido-completing-read)
+(setq find-project-default-action 'find-file-in-repository)
+(setq find-project-default-action
+      '(magit-status find-file-in-repository find-file find-project-dired))
+
+
+(provide 'setup-find-project)
