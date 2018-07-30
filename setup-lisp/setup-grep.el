13a32a6d8d623cca0cddc33c624628cf9a4a6d2b
Vyacheslav Levit <dev@vlevit.org>
2018-07-30 09:39:13 +0300

    counsel-rg: don't show long lines (not to slow down)

--- setup-lisp/setup-grep.el
+++ setup-lisp/setup-grep.el
@@ -1,5 +1,6 @@
 (eval-after-load 'grep '(add-to-list 'grep-find-ignored-directories "node_modules"))
 
 (setq rg-command-line-flags '("-S -M1000"))
+(setq counsel-rg-base-command "rg --no-heading --line-number -S -M1000 %s .")
 
 (provide 'setup-grep)
