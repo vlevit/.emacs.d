285bed953130d2f590af2e385001150519066e44
Vyacheslav Levit <dev@vlevit.org>
2018-12-22 22:42:23 +0200

    Update font settings

--- setup-lisp/setup-face.el
+++ setup-lisp/setup-face.el
@@ -1,9 +1,9 @@
 (defun my-set-dynamic-face ()
   (interactive)
   (if (>= (x-display-pixel-width) 1920)
-      (set-frame-font "DejaVu Sans Mono 11")
+      (set-frame-font "DejaVu Sans Mono 13")
     (set-frame-font "DejaVu Sans Mono 10")
-    (set-face-attribute 'default nil :height 105)))
+    (set-face-attribute 'default nil :height 130)))
 
 (my-set-dynamic-face)
 
